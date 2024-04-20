d3c1a8224d0b1f9a1136996f4e739ba9ab4ff57d
RuslanGon <ruslan441006@gmail.com>
2024-04-20 11:55:55 +0300

    ert

--- anim.js
+++ anim.js
@@ -1,1 +1,19 @@
-console.log(1234);
+let animeItems = document.querySelectorAll(".anime-items");
+
+if (animeItems.length > 0) {
+  function animOnScroll() {
+    for (let i = 0; i > animeItems.length; i++) {
+      const animeItem = animeItems[i];
+      const animeItemHeight = animeItem.offsetHeight
+      const animeItemOffset = offset(animeItem).top
+      animeStart = 
+    }
+
+    function offset(el){
+        const rest = el.getBoundingClientRect()
+        scrollLeft = window.pageXOffset || document.documentElement.scrollLeft
+        scrollTop = window.pageYOffset || document.documentElement.scrollTop
+        return {top: react.top + scrollTop, left: react.left + scrollLeft}
+    }
+  }
+}
