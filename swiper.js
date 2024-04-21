2662b72df06e1ef33de83efa4b23250cc5064ba3
RuslanGon <ruslan441006@gmail.com>
2024-04-21 13:04:26 +0300

    wer

--- swiper.js
+++ swiper.js
@@ -20,9 +20,13 @@
 //     },
 //   });
 
- new Swiper('.image-slider',{
-  navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
-        },
- })
+ new Swiper(".image-slider", {
+   navigation: {
+     nextEl: ".swiper-button-next",
+     prevEl: ".swiper-button-prev",
+   },
+   pagination: {
+     el: ".swiper-pagination",
+     clickable: true,
+   },
+ });
